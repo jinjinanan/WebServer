317bb0fb1a9e430c2b21d0adb1a5047ccfc25e17
陈林波 <jinjinanan@hotmail.com>
2018-08-13 16:11:28 +0800

    添加route

--- webFramework/frameworkPackage/microFramework.py
+++ webFramework/frameworkPackage/microFramework.py
@@ -1,4 +1,6 @@
+
 from werkzeug.wrappers import Request,Response
+from werkzeug.routing import Map, Rule
 from werkzeug.local import LocalStack,LocalProxy
 
 class CusResponse(Response):
@@ -9,6 +11,9 @@ class framework(object):
     def __init__(self,package_name):
         self.package_name = package_name
         self.debug = False
+        self.view_functions = {}
+        # 路由
+        self.url_map = Map()
 
     def run(self,host='localhost', port=5000, **options):
         from werkzeug import run_simple
@@ -18,11 +23,25 @@ class framework(object):
         options.setdefault('use_debugger', self.debug)
         return run_simple(host,port,self,options)
 
+    def dispatch_request(self):
+        pass
+
     def wsgi_app(self,environ,start_response):
         request = Request(environ)
         response = CusResponse('hello world \n')
         return response(environ,start_response)
 
+    def route(self,rule, **options):
+        def decorator(f):
+            if 'endpoint' is not options:
+                options['endpoint'] = f.__name__
+            self.url_map.add(Rule(rule,**options))
+            self.view_functions[options['endpoint']] = f
+            return f
+        return decorator
+
     def __call__(self, environ,start_response):
         return self.wsgi_app(environ,start_response)
+
+
 
