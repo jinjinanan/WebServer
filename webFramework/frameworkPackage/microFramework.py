be4960b55a7e715e5ec48f4bb847b735a336fa19
陈林波 <jinjinanan@hotmail.com>
2018-08-15 16:16:35 +0800

    添加 路由功能

--- webFramework/frameworkPackage/microFramework.py
+++ webFramework/frameworkPackage/microFramework.py
@@ -2,11 +2,37 @@
 from werkzeug.wrappers import Request,Response
 from werkzeug.routing import Map, Rule
 from werkzeug.local import LocalStack,LocalProxy
+from werkzeug.contrib.securecookie import SecureCookie
+from six import string_types
+import threading
+class CusRequest(Request):
+    def __init__(self):
+        super().__init__()
+        self.endpoint = None
+        self.view_args = None
+
 
 class CusResponse(Response):
     default_mimetype = 'text/html'
 
+class _RequestContext(object):
+    def __init__(self,app,environ):
+        self.app = app
+        self.request = app.request_class
+        self.response = app.response_class
+        self.url_adapter = app.url_map.bind_to_environ(environ)
+        # self.session = app.open_session(self.request)
+
+
 class framework(object):
+
+    request_class = CusRequest
+
+    response_class = CusResponse
+
+    secret_key = None
+
+    session_cookie_name = 'session'
 
     def __init__(self,package_name):
         self.package_name = package_name
@@ -14,6 +40,15 @@ class framework(object):
         self.view_functions = {}
         # 路由
         self.url_map = Map()
+
+    # def open_session(self,request):
+    #     key = self.secret_key
+    #     if key is not None:
+    #         return SecureCookie.load_cookie(request,self.session_cookie_name,secret_key=key)
+    #
+    # def save_session(self,session,response):
+    #     if session is not None:
+    #         session.save_cookie(response,self.session_cookie_name)
 
     def run(self,host='localhost', port=5000, **options):
         from werkzeug import run_simple
@@ -23,13 +58,57 @@ class framework(object):
         options.setdefault('use_debugger', self.debug)
         return run_simple(host,port,self,options)
 
+
+    #  处理请求
+    def match_request(self):
+        rv = _request_ctx_stack.top.url_adapter.match()
+        request.endpoint,request.view_args = rv # ('index', {})
+        import pdb
+        pdb.set_trace()
+        return rv
+
     def dispatch_request(self):
+        endpoints,values = self.match_request()
+        return self.view_functions[endpoints](**values)
+
+    def make_response(self,rv):
+        if isinstance(rv,self.response_class):
+            return rv
+        if isinstance(rv,string_types):
+            return self.response_class(rv)
+
+
+    def process_response(self,response):
+        # session = _request_ctx_stack.top.session
+        # if session is not None:
+        #     self.save_session(session,response)
         pass
 
+
+
     def wsgi_app(self,environ,start_response):
-        request = Request(environ)
-        response = CusResponse('hello world \n')
-        return response(environ,start_response)
+        """
+        :param environ:
+        :param start_response:
+        :return:
+        实际的标准wsgi客户端函数
+        """
+        # request = Request(environ)
+        # response = CusResponse('hello world \n')
+
+        _request_ctx_stack.push(_RequestContext(self, environ))
+        try:
+            rv = self.dispatch_request()
+            response = self.make_response(rv)
+            # response = self.process_response(response)
+            # return response
+
+            # response = CusResponse('hello world \n')
+            # return response(environ, start_response)
+            return response(environ,start_response)
+        finally:
+            _request_ctx_stack.pop()
+
 
     def route(self,rule, **options):
         def decorator(f):
@@ -44,4 +123,6 @@ class framework(object):
         return self.wsgi_app(environ,start_response)
 
 
-
+_request_ctx_stack = LocalStack()
+request = LocalProxy(lambda :_request_ctx_stack.top.request)
+# session = LocalProxy(lambda :_request_ctx_stack.top.session)
